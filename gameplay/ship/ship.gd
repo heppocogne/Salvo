2149ecf89243bbd35dc5866e975683281d985e0b
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-14 11:07:48 +0900

    (wip) Add Ship class

---
+++ gameplay/ship/ship.gd
@@ -0,0 +1,14 @@
+class_name Ship
+extends Area2D
+
+export var base_speed:=0.0
+export var base_hp:=100
+export var protection:Vector2
+
+
+func _ready():
+	pass
+
+
+func _physics_process(_delta:float):
+	pass
