91376e722bdd7ba7d296cfb0a519ab4078e83302
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-15 17:48:37 +0900

    (wip) Add tutorial

--- main/main.gd
+++ main/main.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _ready():
-	get_tree().change_scene_to(preload("res://gameplay/screen/battle_screen.tscn"))
+	get_tree().change_scene_to(preload("res://gameplay/screen/tutorial/tutorial.tscn"))
