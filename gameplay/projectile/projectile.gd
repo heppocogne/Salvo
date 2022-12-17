ffaa989d08f247a990e8c4679119bc7832835827
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-17 17:07:12 +0900

    Add explosion effect

--- gameplay/projectile/projectile.gd
+++ gameplay/projectile/projectile.gd
@@ -26,6 +26,11 @@ func get_damage()->int:
 func _on_Projectile_area_entered(area:Area2D):
 	if area.has_method("damage"):
 		area.damage(self)
+		var explosion:Particles2D=preload("res://gameplay/effect/explosion.tscn").instance()
+		GlobalScript.node2d_root.add_child(explosion)
+		explosion.global_position=global_position
+		explosion.scale=0.05*Vector2(1,1)
+		explosion.speed_scale=2.0
 	elif area==GlobalScript.water_area:
 		var splash:Particles2D=preload("res://gameplay/effect/water_splash.tscn").instance()
 		GlobalScript.node2d_root.add_child(splash)
