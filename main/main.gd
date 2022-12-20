de1c1c3b84036e3ca5544758faa7e08695841dae
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-20 19:18:58 +0900

    Update main screen layout
    Add StageDescription
    Support quit button

--- main/main.gd
+++ main/main.gd
@@ -2,14 +2,17 @@ extends Control
 
 
 func _ready():
-	for b in $Panel/VBoxContainer/HBoxContainer/GridContainer.get_children():
+	for b in $Panel/VBoxContainer/HBoxContainer/MarginContainer2/HBoxContainer.get_children():
 		if SaveData.has_key(b.save_data_key):
-			b.unlocked=SaveData.read(b.save_data_key)
+			b.set_unlocked(SaveData.read(b.save_data_key))
 
 
-func _on_Tutorial_pressed():
-	get_tree().change_scene_to(preload("res://gameplay/screen/tutorial/tutorial.tscn"))
+func _on_Stage_button_pressed(n:String):
+	if n=="Tutorial":
+		get_tree().change_scene_to(preload("res://gameplay/screen/tutorial/tutorial.tscn"))
+	elif n=="Stage 1":
+		get_tree().change_scene_to(preload("res://gameplay/screen/stage_1/stage_1.tscn"))
 
 
-func _on_Stage_1_pressed():
-	get_tree().change_scene_to(preload("res://gameplay/screen/stage_1/stage_1.tscn"))
+func _on_Exit_pressed():
+	get_tree().quit(0)
---
+++ main/stage_description.gd
@@ -0,0 +1,43 @@
+tool
+class_name StageDescription
+extends HBoxContainer
+
+signal button_pressed(n)
+
+export var unlocked:=false setget set_unlocked
+export var save_data_key:String
+export var stage_name:String setget set_stage_name
+export var description:String setget set_description
+
+
+func _ready():
+	set_unlocked(unlocked)
+	set_stage_name(stage_name)
+	set_description(description)
+
+
+func set_unlocked(u:bool):
+	unlocked=u
+	if has_node("Button"):
+		$Button.disabled=!unlocked
+	set_description(description)
+
+
+func set_stage_name(n:String):
+	stage_name=n
+	if has_node("Button"):
+		$Button.text=n
+
+
+func set_description(d:String):
+	description=d
+	if !has_node("Description"):
+		return
+	if unlocked:
+		$Description.text=d
+	else:
+		$Description.text="???"
+
+
+func _on_Button_pressed():
+	emit_signal("button_pressed",stage_name)
