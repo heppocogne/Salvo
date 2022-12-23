caf21a2adb6ce6fdd3eefaad04416416ca2b9dea
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-23 10:34:26 +0900

    Avoid to be saved by editor

--- gameplay/screen/main/lockable_button.gd
+++ gameplay/screen/main/lockable_button.gd
@@ -7,12 +7,12 @@ export var save_data_key:String
 
 
 func _ready():
-	if save_data_key!="":
+	if save_data_key!="" and !Engine.editor_hint:
 		unlocked=SaveData.read(save_data_key)
 
 
 func set_unlocked(u:bool):
 	unlocked=u
 	disabled=!u
-	if save_data_key!="":
+	if save_data_key!="" and !Engine.editor_hint:
 		SaveData.store(save_data_key,true)
