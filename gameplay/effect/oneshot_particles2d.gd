386e74f36b52285e8fad7cd4642bb71f971037a2
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-24 10:12:49 +0900

    Add return after `queue_free()`

--- gameplay/effect/oneshot_particles2d.gd
+++ gameplay/effect/oneshot_particles2d.gd
@@ -20,6 +20,7 @@ func _process(_delta:float):
 		emit_signal("emission_finished")
 		if auto_remove:
 			queue_free()
+			return
 	elif !_prev_emitting and emitting:
 		emit_signal("emission_started")
 	_prev_emitting=emitting
