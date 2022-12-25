cf9bbfa47e04cce8dfdab6bf7bdbf50d2af22a36
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-25 18:23:40 +0900

    Remove small_gun (unused) Rename large_gun to gun
    Reload scene

--- gameplay/weapon/gun/gun.gd
+++ gameplay/weapon/gun/gun.gd
@@ -7,6 +7,6 @@ func _ready():
 
 
 func put_projectile(rot:float,dispersion:float,accuracy:float):
-	if !$AudioStreamPlayer2D.playing:
-		$AudioStreamPlayer2D.play()
+	if !$AudioStreamPlayer.playing:
+		$AudioStreamPlayer.play()
 	.put_projectile(rot,dispersion,accuracy)
