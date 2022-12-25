eec2050bd0764df639ea355ef41a6cdc3889be61
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-26 00:49:15 +0900

    (wip) Implement stage_2 Nerf old_cl

--- gameplay/screen/stage_2/stage_2.gd
+++ gameplay/screen/stage_2/stage_2.gd
@@ -1,8 +1,28 @@
 extends BattleScreen
 
 
+const _old_dd_scene:PackedScene=preload("res://gameplay/ship/enemy/old_dd.tscn")
+const _old_cl_scene:PackedScene=preload("res://gameplay/ship/enemy/old_cl.tscn")
+
+var kill_count:=0
+
+
 func _ready():
 	pass
 
+
 func _on_Timer_timeout():
 	_fadeout_mission_text("敵艦隊を撃沈せよ",4.0)
+	var s:=spawn_enemy_ship(_old_dd_scene,600)
+	s.connect("killed",self,"_on_Enemy_killed")
+	s=spawn_enemy_ship(_old_cl_scene,700)
+	s.connect("killed",self,"_on_Enemy_killed")
+	s=spawn_enemy_ship(_old_cl_scene,800)
+	s.connect("killed",self,"_on_Enemy_killed")
+
+
+func _on_Enemy_killed():
+	kill_count+=1
+	
+	if kill_count==3:
+		pass
