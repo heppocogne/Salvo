6c25df0774e4030cf93d5a5933fb8faf88866dc2
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-12-29 00:30:20 +0900

    Add marker

---
+++ gameplay/misc/marker.gd
@@ -0,0 +1,30 @@
+class_name Marker
+extends Sprite
+
+export var rotation_speed:float=TAU
+export var popup_offset:Vector2
+
+var target_node:Node2D
+var disabled:=false setget set_disabled
+
+var _scale_x:=scale.x
+var _time:=0.0
+
+
+func _ready():
+	pass
+
+
+func _process(delta:float):
+	if weakref(target_node).get_ref():
+		global_position=target_node.global_position+popup_offset
+	else:
+		return
+	if !disabled:
+		_time+=delta
+		scale.x=_scale_x*cos(rotation_speed*_time)
+
+
+func set_disabled(f:bool):
+	disabled=f
+	visible=!f
