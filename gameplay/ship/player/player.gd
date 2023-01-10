1dd6aad3a7980a8284eaa2656df00ae952af6b52
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-01-10 19:05:58 +0900

    Buff player

--- gameplay/ship/player/player.gd
+++ gameplay/ship/player/player.gd
@@ -60,9 +60,7 @@ func _ready():
 			SaveData.read("upgrade_deck_armor")*12.7
 		)
 	
-	_regeneration_per_sec=SaveData.read("upgrade_emergency_repair")
-	if _regeneration_per_sec!=0:
-		_regeneration_per_sec+=1
+	_regeneration_per_sec=SaveData.read("upgrade_emergency_repair")*2
 	_main_weapon_reload_upgrade-=SaveData.read("upgrade_main_weapon_reload")*0.4
 	if weapon_groups.has("main"):
 		# restart reload timer to apply upgrade effect
