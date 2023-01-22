ffdf4d831d98f823f3ca92896db05c87ac5f63f7
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-01-22 10:31:37 +0900

    Remove viewport container

--- gameplay/screen/hp_bar.gd
+++ gameplay/screen/hp_bar.gd
@@ -4,7 +4,7 @@ var player_node:Player
 
 
 func _ready():
-	player_node=$"../../../../../ViewportContainer/Viewport/Node2DRoot/Player"
+	player_node=$"../../../../../../Node2DRoot/Player"
 	max_value=player_node.get_max_hp()
 	value=player_node.hp
 
